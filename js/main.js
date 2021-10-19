36662a307edd3f7705ff1a5d235c2e872a0e7568
Aleksandr <aleksandrsamsonenko0794@gmail.com>
2021-10-19 21:52:56 +0300

    added function

--- js/main.js
+++ js/main.js
@@ -1,33 +1,52 @@
 'use strict'
 
+const features =['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const type = [ 'palace','flat','house','bungalow'];
+const photos= ['http://o0.github.io/assets/images/tokyo/hotel1.jpg','http://o0.github.io/assets/images/tokyo/hotel2.jpg','http://o0.github.io/assets/images/tokyo/hotel3.jpg.'];
+const checkin = ['12:00', '13:00', '14:00'];
+const checkout = ['12:00', '13:00', '14:00'];
+//функция поиска елемента по массиву
+const getElementFromArr = (arr, number) => {
+  const currentElement = arr[number]
+  return currentElement;
+};
+//функция рандомного числа в диапазоне от startNumber до endNumber
+const randomNumber=(startNumber, endNumber)=>{
+  const number = Math.floor(Math.random() * (endNumber - startNumber) + startNumber);
+  return number;
+}
+//Location
+function getLocation(min, max){
+  const calcNum = Math.random() * (max - min) + min;
+  const locationFix = calcNum.toFixed(5);
+  const locationNum = (locationFix)* 1;
+  return locationNum;
+}
+
+
 let arr = new Array(10).fill().map((u,index) => ({
   author:{
-    avatar:'img/avatars/user01.png',
+    avatar:`img/avatars/user0${randomNumber(1,8)}.png`,
   },
   offer:{
     title:'title',
     address: 'address',
     price: '200',
-    type: 'palace',
-    rooms: '5',
-    guests: '5',
-    checkin: '12:00',
-    checkout: '13:00',
-    features:['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    photos:['http://o0.github.io/assets/images/tokyo/hotel1.jpg','http://o0.github.io/assets/images/tokyo/hotel2.jpg','http://o0.github.io/assets/images/tokyo/hotel3.jpg.'],
+    type: getElementFromArr(type, 3),
+    rooms: randomNumber(1,20),
+    guests: randomNumber(1,40),
+    checkin: getElementFromArr(checkin, 1),
+    checkout: getElementFromArr(checkout, 0),
+    features:getElementFromArr(features, 2),
+    photos: getElementFromArr(photos, 1),
   },
   location:{
-    x: 35.65000,
-    y: 139.70000,
+    x: getLocation(35.65000,35.70000),
+    y: getLocation(139.70000,139.80000),
   },
 }
 ));
 console.log(arr)
 
 
-const getElementFromArr = (arr, number) => {
 
-    const currentElement = arr[number]
-    console.log(currentElement)
-  };
-  getElementFromArr(features, 2);
