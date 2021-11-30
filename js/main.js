79a4e114b16419721716cff2279ff66437ea1aa5
Aleksandr <aleksandrsamsonenko0794@gmail.com>
2021-11-30 20:28:44 +0200

    added task4

--- js/main.js
+++ js/main.js
@@ -3,6 +3,24 @@
 import {getElementFromArr, randomNumber, getLocation} from './modules/util.js';
 import {features,type, photos, checkin, checkout,description} from './modules/data.js';
 
+const imageEl= document.querySelector('#card');
+const cloneElement = imageEl.cloneNode(true);
+const wrapper = cloneElement.content.querySelector('.popup');
+const image = wrapper.querySelector('.popup__avatar');
+const text = wrapper.querySelector('.popup__title');
+const dress = wrapper.querySelector('.popup__text--address');
+const cost = wrapper.querySelector('.popup__text--price');
+const valueType = wrapper.querySelector('.popup__type');
+const valueCapacity = wrapper.querySelector('.popup__text--capacity');
+const valueTime = wrapper.querySelector('.popup__text--time');
+const valueFeatures = wrapper.querySelector('.popup__features');
+const valueDescription = wrapper.querySelector('.popup__description');
+const popPhotos = wrapper.querySelector('.popup__photos');
+const canvas = document.querySelector('#map-canvas');
+
+const form = document.querySelector('.ad-form__element--time');
+const formElement = document.querySelector('.ad-form');
+
 let arr = new Array(10).fill().map((u,index) => ({
   author:{
     avatar:`img/avatars/user0${randomNumber(1,8)}.png`,
@@ -35,26 +53,12 @@ const address = firstEl.offer.address;
 const price = firstEl.offer.price + ' ' + '₴/ночь';
 const rooms = firstEl.offer.rooms;
 const guest = firstEl.offer.guests;
-const checki = firstEl.offer.checkin;
-const checkou = firstEl.offer.checkout;
-const feature = firstEl.offer.features;
-const descriptio = firstEl.offer.description;
-const photo = firstEl.offer.photos;
 
-const imageEl= document.querySelector('#card');
-const cloneElement = imageEl.cloneNode(true);
-const wrapper = cloneElement.content.querySelector('.popup');
-const image = wrapper.querySelector('.popup__avatar');
-const text = wrapper.querySelector('.popup__title');
-const dress = wrapper.querySelector('.popup__text--address');
-const cost = wrapper.querySelector('.popup__text--price');
-const valueType = wrapper.querySelector('.popup__type');
-const valueCapacity = wrapper.querySelector('.popup__text--capacity');
-const valueTime = wrapper.querySelector('.popup__text--time');
-const valueFeatures = wrapper.querySelector('.popup__features');
-const valueDescription = wrapper.querySelector('.popup__description');
-const popPhotos = wrapper.querySelector('.popup__photos');
-const canvas = document.querySelector('#map-canvas');
+const timeIn = form.querySelector('#timein');
+const timeOut = form.querySelector('#timeout');
+const elType = formElement.querySelector('#type');
+const elPrice = formElement.querySelector('#price');
+
 
 let typeTex;
 switch (secondType ) {
@@ -80,6 +84,11 @@ valueType.textContent = typeTex;
 valueCapacity.textContent = `${rooms}комнаты для ${guest}гостей`;
 valueTime.textContent = `Заезд после ${checkin[0]}, выезд до ${checkout[2]}`;
 
+timeIn.addEventListener('change',inTime );
+timeOut.addEventListener('change', outTime);
+elType.addEventListener('change',typeEl );
+
+
 let newFeatures = features.map(features=> {
   return` <li class="popup__feature popup__feature--${features}"></li>`
 }).join('');
@@ -93,9 +102,36 @@ popPhotos.innerHTML = newPhotos;
 
 canvas.appendChild(wrapper);
 
+function inTime(evt){
+  const timeIn  = evt.target.value;
+  document.getElementById('timeout').value = timeIn;
+}
 
+function outTime(evt){
+  const timeOut  = evt.target.value;
+  document.getElementById('timein').value = timeOut;
+}
 
+function typeEl(event){
+  const elType = event.target.value;
 
-
+  let minPrice;
+  switch (elType ) {
+    case 'palace':
+      minPrice = 10000;
+      break;
+    case 'flat':
+      minPrice = 1000;
+      break;
+    case 'bungalow':
+      minPrice = 0;
+      break;
+    case 'house':
+      minPrice = 5000;
+      break;
+  }
+  elPrice.min = minPrice;
+  elPrice.placeholder = minPrice;
+}
 
 
