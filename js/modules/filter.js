98e5d231f4d0247513133b056d0a7a9afce196d3
Aleksandr <aleksandrsamsonenko0794@gmail.com>
2022-02-01 01:18:40 +0200

    added filteredOffers

--- js/modules/filter.js
+++ js/modules/filter.js
@@ -23,3 +23,82 @@ export async function formChange() {
   //get data from server
 
 }
+
+const mapFilters = document.querySelector(".map__filters")
+
+mapFilters.addEventListener("change", filter)
+
+export function filter(event, data){
+  debugger
+  let  filteredOffers =[]
+  let bluePin = document.querySelectorAll('[title*="My another Location"]')
+  bluePin.forEach(el => el.remove())
+
+
+  const houseType = document.getElementById("housing-type")
+  const housePrice = document.getElementById("housing-price")
+
+
+  let houseTypeValue = houseType.value
+  let housePriceValue = housePrice.value
+
+
+  function housingTypeHandler(elem){
+
+    if(houseTypeValue === "palace"){
+      return elem.offer.type === "palace"
+    }
+    if(houseTypeValue === "flat"){
+      return elem.offer.type === "flat"
+    }
+    if(houseTypeValue === "house"){
+      return elem.offer.type === "house"
+    }
+    if(houseTypeValue === "bungalow"){
+      return elem.offer.type === "bungalow"
+    }
+    if (houseTypeValue === "any"){
+      return true
+    }
+  }
+
+  function housingPriceHandler(elem){
+
+    if (housePriceValue === "low"){
+      return  elem.offer.price < 10000
+    }
+    if (housePriceValue === "middle"){
+      return elem.offer.price >= 10000 && elem.offer.price <= 50000
+    }
+    if (housePriceValue === "high"){
+      return elem.offer.price > 50000
+    }
+    if (housePriceValue === "any"){
+      return true
+    }
+  }
+
+
+
+  let featuresFilter = function (elem) {
+
+    const filterFeaturesCheckboxes = document.querySelectorAll('.map__features input[type=checkbox]:checked');
+    let filtered = true;
+    if (filterFeaturesCheckboxes.length && elem.offer.features) {
+      filterFeaturesCheckboxes.forEach(function (chBox) {
+        if (!elem.offer.features.includes(chBox.value) ) {
+          filtered = false;
+        }
+      });
+    }
+    return filtered;
+  };
+
+
+
+  let commonFilter = function (elem) {
+    return housingTypeHandler(elem) && housingPriceHandler(elem) && featuresFilter(elem)
+  }
+  filteredOffers = data.filter(commonFilter)
+  getMarkerInfo (filteredOffers)
+}
